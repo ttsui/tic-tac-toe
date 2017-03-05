561710a18e6b54b04491974214f62161720466b5
Tony Tsui <tsui.tony@gmail.com>
2017-03-05 14:33:32 +1100

    Implement winningCoordinates() function.

--- src/GameEngine.js
+++ src/GameEngine.js
@@ -1,3 +1,22 @@
 export function winningCoordinates(gridSize) {
-  return [];
+  let coordinates = [];
+
+  let diagnol1 = [];
+  let diagnol2 = [];
+  for (let i=0; i<gridSize; i++) {
+    diagnol1.push(`${i},${i}`);
+    diagnol2.push(`${i},${(gridSize-1) - i}`);
+
+    let row = [];
+    let col = [];
+    for (let j=0; j<gridSize; j++) {
+      row.push(`${i},${j}`);
+      col.push(`${j},${i}`);
+    }
+
+    coordinates.push(row, col);
+  }
+  coordinates.push(diagnol1, diagnol2);
+
+  return coordinates;
 }
